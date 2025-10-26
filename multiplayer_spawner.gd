68bff87ffa45a688a5bf5c4bd653835de6421f00
polarbearsam <112090381+polarbearsam@users.noreply.github.com>
2025-10-25 22:02:57 -0400

    More Multiplayer

--- multiplayer_spawner.gd
+++ multiplayer_spawner.gd
@@ -4,8 +4,8 @@ extends MultiplayerSpawner
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Peer Connected!")
-	multiplayer.peer_connected.connect(spawn_player)
+	Networking_Handler.connect("player_connected", spawn_player)
+	Networking_Handler.connect("player_disconnected", delete_player)
 
 
 func spawn_player(id: int) -> void:
@@ -15,3 +15,12 @@ func spawn_player(id: int) -> void:
 	player.name = str(id)
 	
 	get_node(spawn_path).call_deferred("add_child", player)
+
+func delete_player(id: int) -> void:
+	if !multiplayer.is_server(): return
+	rpc("_del_player", id)
+
+@rpc("authority", "call_local", "reliable") func _del_player(id: int) -> void:
+	print("Deleting node: " + str(id))
+	if get_node("../"+str(id)) != null:
+		get_node("../"+str(id)).queue_free()
--- networking_handler.gd
+++ networking_handler.gd
@@ -1,25 +1,44 @@
 extends Node
 
-#signal player_connected(peer_id, player_info)
-#signal player_disconnected(peer_id)
-#signal server_disconnected
+signal player_connected(peer_id)
+signal player_disconnected(peer_id)
 
 @export var ip_address: String = "localhost"
-@export var port: int = 1965
+@export var port: int = 2010
 @export var max_clients: int = 5
 
 var peer: ENetMultiplayerPeer
 
 func _ready() -> void:
-	pass
-	#multiplayer.peer_connected.connect(_player_connected)
-	#multiplayer.peer_disconnected.connect()
-	#multiplayer.connected_to_server.connect()
-	#multiplayer.connection_failed.connect()
-	#multiplayer.server_disconnected.connect()
+	multiplayer.peer_connected.connect(_player_connected)
+	multiplayer.peer_disconnected.connect(_player_disconnected)
+	multiplayer.connected_to_server.connect(_server_connected)
+	multiplayer.connection_failed.connect(_connection_failed)
+	multiplayer.server_disconnected.connect(_server_disconnected)
 
-#func _player_connected(peer_id: int) -> void:
-#	player_connected.emit()
+func _player_connected(peer_id: int) -> void:
+	if !multiplayer.is_server(): return
+	print("Player " + str(peer_id) + " connected!")
+	player_connected.emit(peer_id)
+
+func _player_disconnected(peer_id: int) -> void:
+	if !multiplayer.is_server(): return
+	print("Player " + str(peer_id) + " disconnected!")
+	player_disconnected.emit(peer_id)
+
+func _server_connected() -> void:
+	print("Connected to server!")
+
+func _connection_failed() -> void:
+	_remove_multiplayer_peer()
+	print("Failed to connect to server!")
+
+func _server_disconnected() -> void:
+	_remove_multiplayer_peer()
+	print("Server disconnected!")
+
+func _remove_multiplayer_peer():
+	multiplayer.multiplayer_peer = null
 
 func start_server() -> void:
 	print("Starting Server!")
@@ -28,11 +47,12 @@ func start_server() -> void:
 	multiplayer.multiplayer_peer = peer
 	print("Server started on " + ip_address + ":" + str(port))
 
-func start_client() -> bool:
-	print("Connecting to server " + ip_address + ":" + str(port))
+func start_client() -> void:
+	print("Attempting to connect to " + ip_address + ":" + str(port))
 	peer = ENetMultiplayerPeer.new()
-	peer.create_client(ip_address, port)
-	multiplayer.multiplayer_peer = peer
-	print("Connected to server!")
-	return true
+	if peer.create_client(ip_address, port) == Error.OK:
+		multiplayer.multiplayer_peer = peer
+		print("Connecting...")
+	else:
+		print("Failed to create client!")
 	
